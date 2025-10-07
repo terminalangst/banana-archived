f34ce64689eadf8a1bf9c9875d34691d7dc8a3d7
oofie <143152154+oofiewav@users.noreply.github.com>
2025-10-06 22:26:44 -0500

    fix to funkin soundtray acting weird

--- src/objects/FunkinSoundTray.hx
+++ src/objects/FunkinSoundTray.hx
@@ -82,29 +82,33 @@ class FunkinSoundTray extends FlxSoundTray
     y = MathUtil.smoothLerpPrecision(y, lerpYPos, ms / 1000, 0.768);
     alpha = MathUtil.smoothLerpPrecision(alpha, alphaTarget, ms / 1000, 0.307);
 
-    // If it has volume, we want to auto-hide after 1 second (1000ms), we simply decrement a timer
-    var hasVolume:Bool = (!FlxG.sound.muted && FlxG.sound.volume > 0);
-
-    if (hasVolume)
+    // Animate sound tray thing
+    if (_timer > 0)
     {
-      // Animate sound tray thing
-      if (_timer > 0)
-      {
-        _timer -= (ms / 1000);
-      }
-      else if (y >= -height)
-      {
-        lerpYPos = -height - 10;
-        alphaTarget = 0;
-      }
-
-      if (y <= -height)
-      {
-        visible = false;
-        active = false;
-      }
+      _timer -= (ms / 1000);
+      alphaTarget = 1;
     }
-    else if (!visible) moveTrayMakeVisible();
+    else if (y >= -height)
+    {
+      lerpYPos = -height - 10;
+      alphaTarget = 0;
+    }
+
+    if (y <= -height)
+    {
+      visible = false;
+      active = false;
+
+      #if FLX_SAVE
+      // Save sound preferences
+      if (FlxG.save.isBound)
+      {
+        FlxG.save.data.mute = FlxG.sound.muted;
+        FlxG.save.data.volume = FlxG.sound.volume;
+        FlxG.save.flush();
+      }
+      #end
+    }
   }
 
   /**
@@ -116,56 +120,36 @@ class FunkinSoundTray extends FlxSoundTray
    */
   override public function show(up:Bool = false):Void
   {
-    moveTrayMakeVisible(up);
-    saveVolumePreferences();
-  }
-
-  function moveTrayMakeVisible(up:Bool = false):Void
-  {
     _timer = 1;
     lerpYPos = 10;
     visible = true;
     active = true;
-    alphaTarget = 1;
+    var globalVolume:Int = Math.round(FlxG.sound.volume * 10);
 
-    for (i in 0..._bars.length)
-      _bars[i].visible = i < getGlobalVolume(up);
-  }
-
-  /**
-   * Calculates the volume with proper linear scaling, and returns it as an int.
-   * @param up Whether the volume is increasing.
-   * @return Int The volume as an int from 0 to 10.
-   */
-  function getGlobalVolume(up:Bool = false):Int
-  {
-		var globalVolume:Int = Math.round(FlxG.sound.volume * 10);
-
-    if (FlxG.sound.muted || FlxG.sound.volume == 0) globalVolume = 0;
+    if (FlxG.sound.muted)
+    {
+      globalVolume = 0;
+    }
 
     if (!silent)
     {
-      // This is a String currently, but there is or was a Flixel PR to change this to a FlxSound or a Sound bject
-      var sound:String = up ? volumeUpSound : volumeDownSound;
+      var sound = up ? volumeUpSound : volumeDownSound;
 
       if (globalVolume == 10) sound = volumeMaxSound;
-      if (sound != null) FlxG.sound.load(sound).play().volume = 0.3;
+
+      if (sound != null) FlxG.sound.load(sound).play();
     }
 
-    return globalVolume;
-  }
-
-  function saveVolumePreferences():Void
-  {
-    // Actually save when the volume is changed / modified
-    #if FLX_SAVE
-    // Save sound preferences
-    if (FlxG.save.isBound)
+    for (i in 0..._bars.length)
     {
-      FlxG.save.data.mute = FlxG.sound.muted;
-      FlxG.save.data.volume = FlxG.sound.volume;
-      FlxG.save.flush();
+      if (i < globalVolume)
+      {
+        _bars[i].visible = true;
+      }
+      else
+      {
+        _bars[i].visible = false;
+      }
     }
-    #end
   }
 }
